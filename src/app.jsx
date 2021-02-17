34e497de11cb8bd8ec843719bf8ed8397ea1ba29
AlbertoParente <albertoparente20@gmail.com>
2021-02-16 23:52:13 -0300

    Adding card component

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import First from './components/basic/First'
 import WithParameter from './components/basic/WithParameter'
 import Fragment from './components/basic/Fragment'
+import Random from './components/basic/Random'
+import Card from './components/layout/Card'
 
 export default () => {
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Card title='Card Example' />
+        <Random min={1} max={60} />
         <Fragment />
         <WithParameter
             title="Situação do Aluno"
