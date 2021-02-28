969730052c764fc722d9431edbabd6eceab2abaa
AlbertoParente <albertoparente20@gmail.com>
2021-02-27 23:52:21 -0300

    Adding the concept of conditional rendering, continuation

--- src/components/conditional/IfElse.js
+++ src/components/conditional/IfElse.js
@@ -1,7 +1,11 @@
 export default props => {
+    const elseChild = props.children
+    
     if(props.test) {
         return props.children
     } else {
         return false
     }
 }
+
+export const Else = props =>props.children
