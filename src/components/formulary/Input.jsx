48e609d623e15f9ce02c4fc7ecc399eb5147f38f
AlbertoParente <albertoparente20@gmail.com>
2021-03-06 21:44:22 -0300

    Adding the concept of controlled component (Input), continuation

--- src/components/formulary/Input.jsx
+++ src/components/formulary/Input.jsx
@@ -2,11 +2,23 @@ import React, { useState } from 'react'
 import "./input.css"
 
 export default props => {
-    const [value, setValue] = useState('Initial')
+    const [value, setValue] = useState('Value: ')
+    
+    function whenToChange(e) {
+        setValue(e.target.value)
+    }
 
     return(
         <div className="Input">
-            <input value={value} />
+            <h2>{value}</h2>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <input value={value} onChange={whenToChange} />
+                <input value={value} readOnly />
+                <input value={undefined} />
+            </div>
         </div>
     )
 }
